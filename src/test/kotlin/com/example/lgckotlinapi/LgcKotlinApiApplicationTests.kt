d901f987dafbd8d0894a30d19edc8139f8c6cf44
valberjr <valber@duck.com>
2024-02-22 17:01:23 -0300

    kotlin api project

---
+++ src/test/kotlin/com/example/lgckotlinapi/LgcKotlinApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.lgckotlinapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LgcKotlinApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
