757d66a181891a8e8b118049e590e767c0e87b84
valberjr <valber@duck.com>
2024-02-23 16:37:06 -0300

    added json property _id

--- src/main/kotlin/com/example/lgckotlinapi/Course.kt
+++ src/main/kotlin/com/example/lgckotlinapi/Course.kt
@@ -1,5 +1,6 @@
 package com.example.lgckotlinapi
 
+import com.fasterxml.jackson.annotation.JsonProperty
 import jakarta.persistence.*
 
 @Entity
@@ -7,6 +8,7 @@ class Course {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @JsonProperty("_id")
     var id: Long = 0
 
     @Column(length = 200, nullable = false)
