378613fa5ba6afb7d717f8d398fe55f0fec4ba4e
valberjr <valber@duck.com>
2024-03-05 10:21:15 -0300

    created findById

--- src/main/kotlin/com/example/lgckotlinapi/controller/CourseController.kt
+++ src/main/kotlin/com/example/lgckotlinapi/controller/CourseController.kt
@@ -3,6 +3,7 @@ package com.example.lgckotlinapi.controller
 import com.example.lgckotlinapi.Course
 import com.example.lgckotlinapi.repository.CourseRepository
 import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController
@@ -12,6 +13,12 @@ class CourseController(private val courseRepository: CourseRepository) {
     @GetMapping
     fun list(): List<Course> =
         courseRepository.findAll()
+
+    @GetMapping("/{id}")
+    fun findById(@PathVariable id: Long): ResponseEntity<Course> =
+        courseRepository.findById(id)
+            .map { record -> ResponseEntity.ok().body(record) }
+            .orElse(ResponseEntity.notFound().build())
 
 
     @PostMapping
